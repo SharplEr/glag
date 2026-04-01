80d311fbee9eb39ddc259bdb8ff5527f51ddfec1
sharpler <sharplermc@gmail.com>
2026-04-01 22:25:44 +0400

    refactoring a bit

---
+++ src/test/java/org/sharpler/glag/records/GcNameTest.java
@@ -0,0 +1,12 @@
+package org.sharpler.glag.records;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GcNameTest {
+
+    @Test
+    void findGcName() {
+    }
+}
